8d4cb6eb237f4055e5990b586ebb6fe02dd2c128
+ <kibetg984@gmail.com>
2024-11-10 16:35:24 +0300

    Made changes on our Code Challenge project

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, setPlants, searchedPlant }) { 
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(data => setPlants(data))
+  }, [])
+
+  const filteredPlants = searchedPlant === "" ? 
+    [...plants] :
+    plants.filter(plant => plant.name.toLowerCase().includes(searchedPlant.toLowerCase()))
+
+  function renderPlants() {
+    return filteredPlants.map((plant, index) => {
+      return <PlantCard key={plant.id || index} plant={plant}/>
+    })
+  }
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {renderPlants()}
+    </ul>
   );
 }
 
